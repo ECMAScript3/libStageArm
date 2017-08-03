d9a1eccccdf3e060a6a5a66e6ce16f54247db40d
Garrett Gu <garrettgu777@gmail.com>
2017-08-03 11:11:06 -0500

    fixed js

--- src/RPI/srvInt/renderer.js
+++ src/RPI/srvInt/renderer.js
@@ -3,54 +3,63 @@
 // All of the Node.js APIs are available in this process.
 
 function isNumeric(n) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
+  return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
+const Dialogs = require('dialogs');
+const SerialPort = require('serialport');
+const Stage = require('./libStageArm.js');
 
 var dialogs = Dialogs();
-var port;
+var stage;
 
 var stepSize = 2;
-var stepSizeInput = document.getElementById('step-size-input');
+var stepSizeInput = document.getElementById('stepSizeInput');
 stepSizeInput.onchange = function() {
-    var newStepSize = stepSizeInput.value;
-    console.log(newStepSize);
-    if(isNumeric(newStepSize)) {
-        stepSize = newStepSize;
-        document.getElementById('step-size-disp').innerHTML = stepSize;
-    }
+  var newStepSize = stepSizeInput.value;
+  console.log(newStepSize);
+  if(isNumeric(newStepSize)) {
+    stepSize = newStepSize;
+    document.getElementById('stepSizeDisp').innerHTML = stepSize;
+  }
 }
 
 var path = dialogs.prompt("Enter Arduino serial path", "/dev/cu.usbmodem1411", (path) => {
-    port = new SerialPort(path);
+  stage = new Stage(path);
 
-    port.on('open', () => {
-        console.log('open');
-        
-        document.addEventListener('keydown', (event) => {   
-            switch(event.keyCode) {
-            case 75: //k
-            case 38: //up
-                relMov(0, stepSize, 0);
-                break;
-            case 74: //j
-            case 40: //down
-                relMov(0, -stepSize, 0);
-            break;
-            case 72: //h
-            case 37: //left
-                relMov(-stepSize, 0, 0);
-                break;
-            case 76: //l
-            case 39: //right
-                relMov(stepSize, 0, 0);
-                break;
-            default:
-                console.log("error bad keycode");
-            }
+  stage.port.on('open', () => {
+    console.log('open');
 
-        })
-    });
+    document.addEventListener('keydown', (event) => {
+      switch(event.keyCode) {
+      case 75: //k
+      case 38: //up
+        stage.relMove(0, stepSize, 0);
+        break;
+      case 74: //j
+      case 40: //down
+        stage.relMove(0, -stepSize, 0);
+      break;
+      case 72: //h
+      case 37: //left
+        stage.relMove(-stepSize, 0, 0);
+        break;
+      case 76: //l
+      case 39: //right
+        stage.relMove(stepSize, 0, 0);
+        break;
+      case 17: //ctrl
+      case 83: //s
+        stage.relMove(0, 0, -stepSize);
+        break;
+      case 16: //shift
+      case 87: //w
+        stage.relMove(0, 0, stepSize);
+        break;
+      default:
+        console.log("error bad keycode");
+      }
+
+    })
+  });
 });
-
-
